378aa28ef1ad727c271edec44ded14dfed8d93a9
Joshua <jssmiller94@gmail.com>
2025-02-12 22:07:34 +0000

    sign off day 2

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ class Mandlebrot:
 
         # instantiate 2d grid (512 x 512) bounded by given values
         self.real_axis = np.linspace(real_lower, real_upper, 512)
-        self.imaginary_axis = np.linspace(imaginary_lower, imaginary_upper, 512)
+        self.imaginary_axis = np.linspace(
+            imaginary_lower, imaginary_upper, 512
+        )
 
+        # for complex values
         self.XX, self.YY = np.meshgrid(self.real_axis, self.imaginary_axis)
-
         self.complex_number = self.XX + self.YY*1j
+
+        # empty grid of zeros that will be updated with corresponding n value 
+        self.mandlebrot_grid = np.zeros((512, 512))
 
     """
     function to calculate how many iterations of n it takes for |z_n| > 2
@@ -31,14 +36,17 @@ class Mandlebrot:
         iterate through 2d grid with x and y coordinates representing complex 
         numbers c (512x512 means 262144 iterations)
 
-            if |z_n| > 2 where z_(n+1) = z_n^2 + c, then set the grid coordinate
-            attribute -number of iterations- to n ie n = 30 
+            if |z_n| > 2 where z_(n+1) = z_n^2 + c, then set the 
+            grid coordinate attribute -number of iterations- to n ie n = 30 
         """
+        # set z_n to equal a 512 by 512 grid of zeros
+        z_n = self.mandlebrot_grid  
+        z_n = z_n**2 + self.complex_number
+        if abs(z_nplus1) > 2:
+            # take x and y coordinate for self.complex_number 
 
-        # z_n = 0
-        # z_n = z_n**2 + self.complex_number
 
-        print(self.complex_number)
+
 
 
 
